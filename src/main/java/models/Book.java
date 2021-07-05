a0cb913ac5539114f9e1c627bf2bab020252db24
Zachary Kitto <zmkitto11@gmail.com>
2021-07-05 09:36:27 -0400

    added book properties

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Book {
+    private int quantity;
+    private double price;
+    private String genre;
+    private String name;
+    private String author;
+    private int id;
 }
